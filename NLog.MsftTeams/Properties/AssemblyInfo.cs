16aecd8abe300a438c5f441e71ebb4a9dae2e885
Joseph Schick <jschick04@gmail.com>
2017-06-13 11:15:19 -0500

    Created Nuget files for packaging

--- NLog.MsftTeams/Properties/AssemblyInfo.cs
+++ NLog.MsftTeams/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLog.MsftTeams")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NLog webhook for Microsoft Teams")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLog.MsftTeams")]
